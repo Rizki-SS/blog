c719fd8642dba1930384c7a77b04eccaf8f09db0
isak <muhammadisak.rizki@gmail.com>
2020-06-26 04:57:06 +0700

    admin lte

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,9 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['prefix' => 'admin', 'middleware' => ['auth']], function () {
+    Route::get('/', function () {
+        return view('dashboard');
+    });
     Route::resource('roles', 'RoleController');
     Route::resource('users', 'UserController');
 });
